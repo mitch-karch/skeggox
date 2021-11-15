b1526420e122b6b908cca98f91363ca79ffa7353
Mitchell Karchemsky <mitch.karchemsky@gmail.com>
2021-11-14 18:36:53 -0800

    Added Last Capture window to UI

--- sandbox/interface-server/camera_interface.py
+++ sandbox/interface-server/camera_interface.py
@@ -1,35 +1,35 @@
 import cv2
 from uuid import uuid4
+
 # raspivid -n -w 1024 -h 768 -t 0 -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink host=192.168.0.100 port=9000
 
-class VideoCamera(object):
 
+class VideoCamera(object):
     def __init__(self):
-        #capturing video
+        # capturing video
         self.ds_factor = 1
         self.video = cv2.VideoCapture(
             'udpsrc port=9000 caps="application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264"'
-            '! rtph264depay'
-            '! video/x-h264,width=1024,height=768,framerate=30/1'
-            '! h264parse'
-            '! avdec_h264'
-            '! videoconvert'
-            '! appsink ',
+            "! rtph264depay"
+            "! video/x-h264,width=1024,height=768,framerate=30/1"
+            "! h264parse"
+            "! avdec_h264"
+            "! videoconvert"
+            "! appsink ",
             cv2.CAP_GSTREAMER,
         )
 
     def __del__(self):
-    #releasing camera
+        # releasing camera
         self.video.release()
 
     def get_frame(self):
-    #extracting frames
-        #ret, frame = self.video.read()
-        #frame=cv2.resize(frame,None,fx=self.ds_factor,fy=self.ds_factor,interpolation=cv2.INTER_AREA)                    
+        # extracting frames
+        # ret, frame = self.video.read()
+        # frame=cv2.resize(frame,None,fx=self.ds_factor,fy=self.ds_factor,interpolation=cv2.INTER_AREA)
 
         ret, frame = self.video.read()
 
-
         # encode OpenCV raw frame to jpg and displaying it
-        ret, jpeg = cv2.imencode('.jpg', frame)
+        ret, jpeg = cv2.imencode(".jpg", frame)
         return jpeg.tobytes()
--- sandbox/interface-server/flask_server.py
+++ sandbox/interface-server/flask_server.py
@@ -1,5 +1,4 @@
-# main.py
-# import the necessary packages
+# Skeggox Interface Server
 from uuid import uuid4
 from flask import Flask, render_template, Response
 
@@ -8,50 +7,53 @@ from camera_interface import VideoCamera
 frame = None
 
 app = Flask(__name__)
-@app.route('/')
+
+
+@app.route("/")
 def index():
     # rendering webpage
-    return render_template('index.html' , commands=AVAILABLE_COMMANDS)
+    return render_template("index.html", commands=AVAILABLE_COMMANDS)
+
 
 def gen(camera):
     global frame
     while True:
-        #get camera frame
+        # get camera frame
         frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n\r\n")
 
-@app.route('/video_feed')
+
+@app.route("/video_feed")
 def video_feed():
-    return Response(gen(VideoCamera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(
+        gen(VideoCamera()), mimetype="multipart/x-mixed-replace; boundary=frame"
+    )
 
 
 ONE, THREE, FIVE, LC, RC, ZERO, EMPTY = "1", "3", "5", "LC", "RC", "0", "E"
 AVAILABLE_COMMANDS = {
-    'One': ONE,
-    'Three': THREE,
-    'Five': FIVE,
-    'Left Clutch': LC,
-    'Right Clutch': RC,
-    'Zero': ZERO,
-    'Empty': EMPTY
+    "1": ONE,
+    "3": THREE,
+    "5": FIVE,
+    "L Clutch": LC,
+    "R Clutch": RC,
+    "0": ZERO,
+    "Empty": EMPTY,
 }
 
-@app.route('/<cmd>')
+
+@app.route("/<cmd>")
 def command(cmd=None):
     global frame
 
-    file_string = f"caps/{uuid4()}_{cmd}.jpg"
-
-    response = f"Capturing {file_string}"
-
-    with open(file_string, 'wb') as f:
+    file_string = f"static/caps/{uuid4()}_{cmd}.jpg"
+    with open(file_string, "wb") as f:
         f.write(frame)
 
-    # ser.write(camera_command)
-    return response, 200, {'Content-Type': 'text/plain'}
+    response = file_string
+    return response, 200, {"Content-Type": "text/plain"}
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # defining server ip address and port
-    app.run(host='0.0.0.0',port='8000', debug=True)
+    app.run(host="0.0.0.0", port="8000", debug=True, threaded=True)
