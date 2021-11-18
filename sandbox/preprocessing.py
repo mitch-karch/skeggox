1e6a77050672e4748d73ac92d83c87cb154ab347
Lynn Marciano <marcianolynn26@gmail.com>
2021-11-18 17:55:07 -0600

    edges are being detected more prominently with using the threshold for edge detection. TODO extend hough lines according to angle and remove horizontal hough lines

--- sandbox/preprocessing.py
+++ sandbox/preprocessing.py
@@ -35,6 +35,7 @@ def draw_detected_edge(img_color):
     img_color = imutils.resize(img_color, height = 500)
     img = imutils.resize(img, height = 500)
     
+    h, w = img.shape[:2]
     
     histr = cv2.calcHist([img],[0],None,[256],[0,256])
     plt.plot(histr)
@@ -46,46 +47,60 @@ def draw_detected_edge(img_color):
     if (k_size%2) == 0:
         k_size +=1
     
+    kernel = (21,21)
+    
     #apply median blur to img
     #median blur computs the median of al the pixels under the kernel window & the central pixed is replaced with this median value
     # blur = cv2.medianBlur(img,ksize=k_size)
-    blur = cv2.GaussianBlur(img, (15,15), 0)
+    blur = cv2.GaussianBlur(img, kernel, 0)
     
     plt.imshow(blur, cmap='gray')
     plt.axis('off')
     
+
     
     #apply a thresholding function to the median blur first before laplacian
     #for every pixel, the same threshold is applied, if the pixel val is smaller than the threshold, it is 0, otherwise the max
     #second arg = thresholdd value used to classify pixel values
     #third arg = max value used to assign to pixel values exceeding threshold
-    _, thresh = cv2.threshold(blur, 145, 255, cv2.THRESH_BINARY_INV)
+    _, thresh = cv2.threshold(blur, 175, 255, cv2.THRESH_BINARY_INV)
     
-    img_edges = cv2.Canny(blur, 100, 100, apertureSize=3)
+    
+    img_edges = cv2.Canny(thresh, 100, 100, apertureSize=7)
     # img_edges = cv2.Canny(thresh, 100, 100, apertureSize=3)
     plt.imshow(img_edges)
     
     #get the hough lines for the edges
-    lines = cv2.HoughLinesP(img_edges, 1, math.pi / 180.0, 100, minLineLength=75, maxLineGap=10)
+    lines = cv2.HoughLinesP(img_edges, 1, math.pi / 180.0, 100, minLineLength=50, maxLineGap=20)
     
     
     #show the hough lines found
     for [[x1, y1, x2, y2]] in lines:
-        # line(img, pt1, pt2, color[, thickness[, lineType[, shift]]])
+    #     # line(img, pt1, pt2, color[, thickness[, lineType[, shift]]])
         cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 5)
-        # cv2.circle(img, (x1,y1), 3, (0,150,0), -1) #line start
-        # cv2.circle(img, (x2,y2), 3, (128,0,0), -1) #line end
+    #     # cv2.circle(img, (x1,y1), 3, (0,150,0), -1) #line start
+    #     # cv2.circle(img, (x2,y2), 3, (128,0,0), -1) #line end
         cv2.putText(img, str(x1) + ',' + str(y1), (x1,y1), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
         cv2.putText(img, str(x2) + ',' + str(y2), (x2,y2), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
-        # angle = math.degrees(math.atan2(y2 - y1, x2 - x1))
-        # angles.append(angle)
+    #     # angle = math.degrees(math.atan2(y2 - y1, x2 - x1))
+    #     # angles.append(angle)
     
     
     
     #get the coordinates of all the lines
     coords = list(zip(lines[:,:,0].flatten().tolist(),lines[:,:,1].flatten().tolist()))
     coords.extend(list(zip(lines[:,:,2].flatten().tolist(),lines[:,:,3].flatten().tolist())))
-    coords.sort()
+    
+    #assume that the edge will never surpass a third of the board
+    coords = [(x,y) for (x,y) in coords if x < w//3]
+    #assume the bottome of the edge will never surpass 480
+    coords = [(x,y) for (x,y) in coords if y < 480]
+    
+    
+    edgepoint_a = min(coords, key=sum)
+    edgepoint_b = max(coords, key=sum)
+    
+    
     
     # #find the minimum x value and max y value to see where the edge of the board is
     # xmin = int(min(lines[:,:,0].min(axis=0), lines[:,:,2].min(axis=0)))
@@ -116,7 +131,7 @@ def draw_detected_edge(img_color):
     # cv2.line(img, (x1, y1), (x2, y2), (0, 0, 0), 3)
     
     #make a line from the min and max points
-    cv2.line(img, coords[0], coords[-1], (0, 0, 0), 3)
+    cv2.line(img, edgepoint_a, edgepoint_b, (0, 0, 0), 3)
     
     cv2.imshow("Detected lines", img) 
     
@@ -127,41 +142,47 @@ for i in range(0,10):
     img_color = 'imgs/img' + str(i) + '.png'
     draw_detected_edge(img_color)
 
-#determine the other aspects of the board based on our line
-length = y2-y1
-
-
-def getPerpCoord(aX, aY, bX, bY, length):
-    vX = bX-aX
-    vY = bY-aY
-    #print(str(vX)+" "+str(vY))
-    if(vX == 0 or vY == 0):
-        return 0, 0, 0, 0
-    mag = math.sqrt(vX*vX + vY*vY)
-    vX = vX / mag
-    vY = vY / mag
-    temp = vX
-    vX = 0-vY
-    vY = temp
-    cX = bX + vX 
-    cY = bY + vY 
-    dX = bX - vX * length
-    dY = bY - vY * length
-    return int(cX), int(cY), int(dX), int(dY)
-
-cX, cY, dX, dY = getPerpCoord(x1, y1, x2, y2, length)
-cv2.line(img, (cX, cY), (dX, dY), (0, 0, 0), 3)
-
-
-# cv2.rectangle(img, (x1, y1), (dX, dY), (255,0,0), 6)
 
 
 
-# median_angle = np.median(angles)
-print(f"Angle is {angle:.04f}")
-img_rotated = ndimage.rotate(img, angle)
-cv2.imshow('rotated-img', img_rotated)
+
+
+
+# #determine the other aspects of the board based on our line
+# length = y2-y1
+
+
+# def getPerpCoord(aX, aY, bX, bY, length):
+#     vX = bX-aX
+#     vY = bY-aY
+#     #print(str(vX)+" "+str(vY))
+#     if(vX == 0 or vY == 0):
+#         return 0, 0, 0, 0
+#     mag = math.sqrt(vX*vX + vY*vY)
+#     vX = vX / mag
+#     vY = vY / mag
+#     temp = vX
+#     vX = 0-vY
+#     vY = temp
+#     cX = bX + vX 
+#     cY = bY + vY 
+#     dX = bX - vX * length
+#     dY = bY - vY * length
+#     return int(cX), int(cY), int(dX), int(dY)
+
+# cX, cY, dX, dY = getPerpCoord(x1, y1, x2, y2, length)
+# cv2.line(img, (cX, cY), (dX, dY), (0, 0, 0), 3)
+
+
+# # cv2.rectangle(img, (x1, y1), (dX, dY), (255,0,0), 6)
+
+
+
+# # median_angle = np.median(angles)
+# print(f"Angle is {angle:.04f}")
+# img_rotated = ndimage.rotate(img, angle)
+# cv2.imshow('rotated-img', img_rotated)
    
 
-key = cv2.waitKey(0)
+# key = cv2.waitKey(0)
 
