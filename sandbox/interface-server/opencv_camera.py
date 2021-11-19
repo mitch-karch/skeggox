646a4ba89ddd148142a6345ace7d36e1d6855493
Mitchell Karchemsky <mitch.karchemsky@gmail.com>
2021-11-18 16:44:03 -0800

    Utilize faster turbojpeg library

--- sandbox/interface-server/opencv_camera.py
+++ sandbox/interface-server/opencv_camera.py
@@ -1,6 +1,8 @@
 import cv2
+from turbojpeg import TurboJPEG
 import time
 from base_camera import BaseCamera
+
 
 class Camera(BaseCamera):
     def __init__(self):
@@ -8,21 +10,21 @@ class Camera(BaseCamera):
 
     @staticmethod
     def frames():
+        jpeg = TurboJPEG()
+
         camera = cv2.VideoCapture(
             'udpsrc port=9000 caps="application/x-rtp, media=(string)video, payload=(int)96, clock-rate=(int)90000, encoding-name=(string)H264"'
-            "! rtpjitterbuffer"
             "! rtph264depay"
-            "! video/x-h264,width=1024,height=768,framerate=30/1"
+            "! video/x-h264,width=1024,height=768,framerate=25/1"
             "! h264parse"
             "! nvv4l2decoder"
             "! nvvidconv"
-            "! video/x-raw, format=BGRx "
-            "! videoconvert"
-            "! video/x-raw, format=BGR"
+            "! video/x-raw, format=(string)I420"
             "! appsink ",
-            cv2.CAP_GSTREAMER,
         )
 
         while True:
-            _,img = camera.read()
-            yield cv2.imencode('.jpg',img)[1].tobytes()
+            _, img = camera.read()
+            img = cv2.cvtColor(img, cv2.COLOR_YUV2BGR_I420)
+            # yield cv2.imencode(".jpg", img)[1].tobytes()
+            yield jpeg.encode(img)
