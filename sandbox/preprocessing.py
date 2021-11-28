291dd82313d0920c5cc3f096f2a4bfa78df8a74f
Lynn Marciano <marcianolynn26@gmail.com>
2021-11-28 16:56:29 -0600

    remove unused code

--- sandbox/preprocessing.py
+++ sandbox/preprocessing.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Mon Nov 15 17:45:19 2021
 
@@ -9,15 +8,9 @@ Created on Mon Nov 15 17:45:19 2021
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
-from skimage.restoration import denoise_nl_means, estimate_sigma
-from skimage import io, img_as_float
-import argparse
 import imutils
 import math
-from scipy import ndimage
-from scipy import stats
 import time
-
 
 
 def draw_detected_edge(img_color):    
@@ -52,9 +45,7 @@ def draw_detected_edge(img_color):
     
     kernel = (21,21)
     
-    #apply median blur to img
-    #median blur computs the median of al the pixels under the kernel window & the central pixed is replaced with this median value
-    # blur = cv2.medianBlur(img,ksize=k_size)
+    #apply gaussian blur
     blur = cv2.GaussianBlur(img, kernel, 0)
     
     plt.imshow(blur, cmap='gray')
@@ -84,19 +75,6 @@ def draw_detected_edge(img_color):
     lines = cv2.HoughLinesP(img_edges, 1, math.pi / 180.0, 100, minLineLength=100, maxLineGap=20)
     
     
-    #show the hough lines found
-    # for [[x1, y1, x2, y2]] in lines:
-    # #     # line(img, pt1, pt2, color[, thickness[, lineType[, shift]]])
-    #     cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 5)
-    # #     # cv2.circle(img, (x1,y1), 3, (0,150,0), -1) #line start
-    # #     # cv2.circle(img, (x2,y2), 3, (128,0,0), -1) #line end
-    #     cv2.putText(img, str(x1) + ',' + str(y1), (x1,y1), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
-    #     cv2.putText(img, str(x2) + ',' + str(y2), (x2,y2), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
-    # #     # angle = math.degrees(math.atan2(y2 - y1, x2 - x1))
-    # #     # angles.append(angle)
-    
-    
-    
     #get the coordinates of all the lines
     coords = list(zip(lines[:,:,0].flatten().tolist(),lines[:,:,1].flatten().tolist()))
     coords.extend(list(zip(lines[:,:,2].flatten().tolist(),lines[:,:,3].flatten().tolist())))
@@ -113,50 +91,36 @@ def draw_detected_edge(img_color):
     length = y2-y1
     
     
-    angle = math.degrees(math.atan2(y2-y1, x2-x1))
+    # angle = math.degrees(math.atan2(y2-y1, x2-x1))
     
     #slope
     slope = (y1-y2)/(x1-x2)
     #y-intercept
     b = (x1*y2 - x2*y1)/(x1-x2)
-    
-    print(f'm={slope}, b={b}')
-    
-    #y = m*x + b
+        
     def get_startpoint(x, y):
         #point slope formula to find the end point if the line segment is too short on top
         while y > h//6:
-            print('minpoint', x, y)
-            x -= int(slope/2)
+            x -= 1
             y = int(slope*x + b)
+            #if y is negative, we want the startpoint to return back to its positive state
             if y < 0:
-               x += int(slope*2) 
+               x += 1
                y = int(slope*x + b)
         return x, y   
         
     def get_endpoint(x, y):
         #point slope formula to find the end point if the line segment is too short on bottom     
-        while y < h//1.1:
-            print('maxpoint', x,y)
-            y += int(slope)
+        while y < h*.9:
+            y += 1
             x = int((y/slope) - (b/slope))
         return x, y
     
-    
-    if length < h//1.1:
+    #if the length of the edge detected is less than 90% of the img
+    if length < h*.9:
         x_start, y_start = get_startpoint(x1, y1)
-        
         x_end, y_end = get_endpoint(x2, y2)
 
-    
-    # cv2.line(img, (x1, y1), (x2, y2), (0, 0, 0), 3)
-    
-    #make a line from the min and max points
-    # cv2.line(img, (x1, y1), (x2, y2), (0, 0, 0), 3)
-    
-    
-    # cv2.putText(img, 'startpoint', (x_start,y_start), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
-    # cv2.putText(img, 'endpoint', (x_end,y_end), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
     
     #make a line from the start and end points
     # cv2.line(img, (x_start,y_start), (x_end,y_end), (255, 0, 0), 5)
@@ -176,23 +140,15 @@ def draw_detected_edge(img_color):
         temp = vX
         vX = 0-vY
         vY = temp
-        cX = bX + vX 
-        cY = bY + vY 
         dX = bX - vX * length
         dY = bY - vY * length
-        print(f'bottom right corner is {dX}, {dY}')
         return int(cX), int(cY), int(dX), int(dY)
     
-    cX, cY, dX, dY = getPerpCoord(x_start,y_start, x_end,y_end, new_length+10)
+    cX, cY, dX, dY = getPerpCoord(x_start,y_start, x_end,y_end, new_length+20)
     
     tX = x_start + (dX - x_end)
     tY = y_start + (dY - y_end)
-    
-    # cv2.line(img, (cX, cY), (dX, dY), (0, 0, 0), 3)
-    # cv2.putText(img, f'{dX}, {dY}', (dX,dY), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
-    # cv2.putText(img, f'{tX}, {tY}', (tX,tY), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
-    
-    
+
     
     def order_points(pts):
     	# initialzie a list of coordinates that will be ordered
@@ -265,47 +221,4 @@ for i in range(0,10):
     img_color = 'imgs/img' + str(i) + '.png'
     draw_detected_edge(img_color)
 
-
-
-
-
-
-
-# #determine the other aspects of the board based on our line
-# length = y2-y1
-
-
-# def getPerpCoord(aX, aY, bX, bY, length):
-#     vX = bX-aX
-#     vY = bY-aY
-#     #print(str(vX)+" "+str(vY))
-#     if(vX == 0 or vY == 0):
-#         return 0, 0, 0, 0
-#     mag = math.sqrt(vX*vX + vY*vY)
-#     vX = vX / mag
-#     vY = vY / mag
-#     temp = vX
-#     vX = 0-vY
-#     vY = temp
-#     cX = bX + vX 
-#     cY = bY + vY 
-#     dX = bX - vX * length
-#     dY = bY - vY * length
-#     return int(cX), int(cY), int(dX), int(dY)
-
-# cX, cY, dX, dY = getPerpCoord(x1, y1, x2, y2, length)
-# cv2.line(img, (cX, cY), (dX, dY), (0, 0, 0), 3)
-
-
-# # cv2.rectangle(img, (x1, y1), (dX, dY), (255,0,0), 6)
-
-
-
-# # median_angle = np.median(angles)
-# print(f"Angle is {angle:.04f}")
-# img_rotated = ndimage.rotate(img, angle)
-# cv2.imshow('rotated-img', img_rotated)
-   
-
-# key = cv2.waitKey(0)
 
