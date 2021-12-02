a341da682a381596b0409afe21f19523843ff67a
Mitchell Karchemsky <mitch.karchemsky@gmail.com>
2021-12-02 14:55:58 -0800

    Moving interface around to the top

--- interface-server/opencv_camera.py
+++ interface-server/opencv_camera.py
@@ -1,5 +1,5 @@
 import cv2
-from turbojpeg import TurboJPEG
+#from turbojpeg import TurboJPEG
 import time
 from base_camera import BaseCamera
 
@@ -10,21 +10,23 @@ class Camera(BaseCamera):
 
     @staticmethod
     def frames():
-        jpeg = TurboJPEG()
+        #jpeg = TurboJPEG()
 
         camera = cv2.VideoCapture(
             'udpsrc port=9000 caps="application/x-rtp, media=(string)video, payload=(int)96, clock-rate=(int)90000, encoding-name=(string)H264"'
             "! rtph264depay"
             "! video/x-h264,width=1024,height=768,framerate=25/1"
             "! h264parse"
-            "! nvv4l2decoder"
-            "! nvvidconv"
-            "! video/x-raw, format=(string)I420"
+            "! avdec_h264"
+            #"! nvv4l2decoder"
+            #"! nvvidconv"
+            "! videoconvert"
+            #"! video/x-raw, format=(string)I420"
             "! appsink ",
         )
 
         while True:
             _, img = camera.read()
-            img = cv2.cvtColor(img, cv2.COLOR_YUV2BGR_I420)
-            # yield cv2.imencode(".jpg", img)[1].tobytes()
-            yield jpeg.encode(img)
+            #img = cv2.cvtColor(img, cv2.COLOR_YUV2BGR_I420)
+            yield cv2.imencode(".jpg", img)[1].tobytes()
+            # yield jpeg.encode(img)
