867c858011254813e1068c4e702313e23854ed8c
Mitchell Karchemsky <mitch.karchemsky@gmail.com>
2021-12-02 10:30:40 -0800

    Quick video capture script

--- sandbox/streaming_test.py
+++ sandbox/streaming_test.py
@@ -3,32 +3,36 @@ import uuid
 import cv2
 from uuid import uuid4
 # raspivid -n -w 1024 -h 768 -t 0 -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink host=192.168.0.100 port=9000
+
 cap = cv2.VideoCapture(
-    'udpsrc port=9000 caps="application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264"'
-    '! rtph264depay'
-    '! video/x-h264,width=1024,height=768,framerate=30/1'
-    '! h264parse'
-    '! avdec_h264'
-    '! videoconvert'
-    '! appsink ',
-    cv2.CAP_GSTREAMER,
+    'udpsrc port=9000 caps="application/x-rtp, media=(string)video, payload=(int)96, clock-rate=(int)90000, encoding-name=(string)H264"'
+    "! rtph264depay"
+    "! video/x-h264,width=1024,height=768,framerate=25/1"
+    "! h264parse"
+    "! avdec_h264"
+    #"! nvv4l2decoder"
+    #"! nvvidconv"
+    "! videoconvert"
+    #"! video/x-raw, format=(string)I420"
+    "! appsink ",
+    cv2.CAP_GSTREAMER
 )
 
 
+writer = cv2.VideoWriter('output.mp4', 0x7634706d, 20.0, (1024,768))
+
 while True:
 
-    ret, frame = cap.read()
-
-    if ret is True:
-        cv2.imshow("receive", frame)
-    else:
-        print("empty frame")
-        continue
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret is True:
+            writer.write(frame)
+        else:
+            print("empty frame")
+            continue
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
-    if cv2.waitKey(1) & 0xFF == ord("1"):
-        cv2.imwrite(f"{uuid4()}_1.jpg", frame)
 cap.release()
 cap.receive()
