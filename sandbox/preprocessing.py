be44117215e12b8755edfd290d55850aa81f2ee5
Lynn Marciano <marcianolynn26@gmail.com>
2021-12-03 20:04:44 -0600

    preprocessing designated training folder

--- sandbox/preprocessing.py
+++ sandbox/preprocessing.py
@@ -11,6 +11,10 @@ from matplotlib import pyplot as plt
 import imutils
 import math
 import time
+from PIL import Image
+
+from os import listdir, mkdir
+from os.path import isfile, join
 
 def normalize_image(img):
     img = cv2.normalize(img, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
@@ -34,13 +38,18 @@ def get_edge_points(lines):
     coords.extend(list(zip(lines[:,:,2].flatten().tolist(),lines[:,:,3].flatten().tolist())))
     
     #assume that the edge will never surpass a quarter of the board
-    coords = [(x,y) for (x,y) in coords if x < w//4]
+    assume_coords = [(x,y) for (x,y) in coords if x < w//4]
     #assume the bottom of the edge will never surpass 480
-    coords = [(x,y) for (x,y) in coords if y < 490]
+    assume_coords = [(x,y) for (x,y) in assume_coords if y < 480]
+    
+    if len(assume_coords) != 0:
+        edge_coords = assume_coords
+    else:
+        edge_coords = coords
     
     #determine initial found edge points
-    x1, y1 = min(coords, key=sum)
-    x2, y2 = max(coords, key=sum)
+    x1, y1 = min(edge_coords, key=sum)
+    x2, y2 = max(edge_coords, key=sum)
     return x1, y1, x2, y2
 
 def get_startpoint(x, y, m, b):
@@ -130,10 +139,35 @@ def four_point_transform(image, pts):
 	# return the warped image
 	return warped
 
-def preprocess(img_color): 
-    global h,w
+
+def write_file(img_to_write):
+    cv2.imwrite(join(scale_crop_dir, img_file), img_to_write)
+   
+def resize_final_img(img):
+    if img.shape[0] > 400 or img.shape[1] > 400:
+        img = img[0:400,0:400]
+    else:
+        img = imutils.resize(img, height = 400, width = 400)
+    return img
+
+#start program
+start_program = time.time()
+
+img_dir = 'grab/imgs/'
+img_files = [f for f in listdir(img_dir) if isfile(join(img_dir, f))]
+
+warped_sizes = []
+
+scale_crop_dir = 'imgs_sc'
+if not isfile(scale_crop_dir):
+    mkdir('imgs_sc')
+
+for img_file in img_files:
+    img_path = join(img_dir+img_file)
+    # preprocess(img_path)
+    
     #load image
-    img_color = cv2.imread(img_color)
+    img_color = cv2.imread(img_path)
     img = cv2.cvtColor(img_color, cv2.COLOR_BGR2GRAY)
     
     #timing
@@ -165,17 +199,35 @@ def preprocess(img_color):
     img_edges = cv2.Canny(morph, 100, 100, apertureSize=7)
     
     #get hough lines from the detected edges
-    lines = cv2.HoughLinesP(img_edges, 1, math.pi / 180.0, 100, minLineLength=100, maxLineGap=20)
+    lines = cv2.HoughLinesP(img_edges, 1, math.pi / 180.0, 100, minLineLength=100, maxLineGap=30)
+    if lines is None: #if no edges can be detected, run it back on the original image
+        lines = cv2.HoughLinesP(blur, 1, math.pi / 180.0, 100, minLineLength=100, maxLineGap=30)
     
     #get the line's edge points to detect the edge of the board
     x1, y1, x2, y2 = get_edge_points(lines)
+    
+    if x1 > w//3:
+        cropped = resize_final_img(img_color)
+        write_file(cropped)
+        continue
     
     #determine the initial length of the detected edge point
     length = y2-y1
     # angle = math.degrees(math.atan2(y2-y1, x2-x1))
     
     #slope
+    if x1-x2 == 0:
+        x1 += 1
+        
     m = (y1-y2)/(x1-x2)
+    if m == 0:
+        m += 1
+    
+    if m < 1:
+        cropped = resize_final_img(img_color)
+        write_file(cropped)
+        continue
+    
     #y-intercept
     b = (x1*y2 - x2*y1)/(x1-x2)
     
@@ -198,20 +250,26 @@ def preprocess(img_color):
     pts = [(tL_x, tL_y), (tR_x, tR_y), (bR_x, bR_y), (bL_x, bL_y)]
     pts = np.array(pts, dtype = "float32")
     warped = four_point_transform(img_color, pts)
+    cropped = resize_final_img(warped)
     
     #timing
     end = time.time()
     print(f'Time elapsed: {end-start}')
     
-    cv2.imshow("Original", img_color) 
-    cv2.imshow("Warped", warped)
+    cv2.imwrite(join(scale_crop_dir, img_file), cropped)
     
+    # cv2.imshow("Original", img_color) 
+    # cv2.imshow("Warped", warped)
+    
+    # key = cv2.waitKey(0)
+
+end_program = time.time()
+print(f'Entire time to process {len(img_files)} images was {end_program-start_program}')
+
+#Testing purposes
+def show_img(img):
+    cv2.imshow('img', img)
     key = cv2.waitKey(0)
-
-#TODO switch data location to box
-#TEST
-for i in range(0,10):
-    img_color = 'imgs/img' + str(i) + '.png'
-    preprocess(img_color)
-
-
+    
+def make_line(x1, y1, x2, y2):
+    cv2.line(img, (x1,y1), (x2,y2), (255, 0, 0), 5)
